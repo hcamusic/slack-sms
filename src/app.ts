eaad67a18b045dc299ffe6dd6c57df312ba6e0a5
Michael Barlock <barlock@ibm.com>
2022-03-25 09:59:35 -0400

    fix: optimize cold-start time so the app will respond faster

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
+import { WebClient } from '@slack/web-api';
 import { App, Receiver } from '@slack/bolt';
 import twilioClient from 'twilio';
 import { HarmonySite } from 'harmonysite';
 import PhoneNumber from 'awesome-phonenumber';
 import _ from 'lodash';
+import {
+  InputBlock,
+  Blocks,
+  PlainText,
+  PlainTextInput,
+  MdSection,
+} from '@slack-wrench/blocks';
 
 const notificationChannel = process.env.NOTIFICATION_CHANNEL || '';
 const messageViewId = 'message';
@@ -13,6 +21,61 @@ const twilio = twilioClient(
 );
 
 const harmonySite = new HarmonySite('https://www.hcamusic.org');
+
+const harmonySiteNumbers = async (): Promise<string[]> => {
+  const hsUsername = process.env.HARMONYSITE_USERNAME || '';
+  const hsPassword = process.env.HARMONYSITE_PASSWORD || '';
+  await harmonySite.authorise(hsUsername, hsPassword);
+
+  const membersTable = await harmonySite.browse({
+    table: 'members',
+    n: 1000,
+  });
+
+  const membershipTable = await harmonySite.browse({
+    table: 'memberships',
+    n: 1000,
+  });
+
+  const memberMap = _.keyBy(membersTable.records.member, (member) => member.id);
+
+  const memberships = _.keyBy(
+    membershipTable.records.membership,
+    (member) => member.Member,
+  );
+
+  const members = membersTable.records.member
+    .filter(
+      (member: any) =>
+        memberships[member.id] &&
+        memberships[member.id].Type === 'Member' &&
+        memberships[member.id].Status === 'Active' &&
+        memberships[member.id].Level === 'Full',
+    )
+    .map((member: any) => {
+      return memberMap[member.id];
+    });
+
+  return members
+    .map(
+      (member: any) =>
+        member.MobilePhone || member.HomePhone || member.WorkPhone,
+    )
+    .filter(_.identity)
+    .filter((phone: any) => typeof phone === 'string')
+    .map((phone: string) => new PhoneNumber(phone, 'US'))
+    .filter((number: PhoneNumber) => number.isValid() && number.isPossible())
+    .map((number: PhoneNumber) => number.getNumber())
+    .filter(_.identity);
+};
+
+const isUserAuthorized = async (client: WebClient, user: string) => {
+  const { members } = await client.conversations.members({
+    channel: notificationChannel,
+  });
+
+  return members && members.includes(user);
+};
 
 export const createApp = (receiver?: Receiver) => {
   const app = new App({
@@ -33,108 +96,53 @@ export const createApp = (receiver?: Receiver) => {
 
     const callingUser = payload.user.id;
 
-    const { members } = await client.conversations.members({
-      channel: notificationChannel,
-    });
-
-    // Check if user is a member of the channel to authorize the request
-    if (members?.includes(callingUser)) {
+    const [openResp, userIsAuthorized] = await Promise.all([
       await client.views.open({
         trigger_id: payload.trigger_id,
         view: {
           callback_id: messageViewId,
-          title: {
-            type: 'plain_text',
-            text: 'SMS the Chorus',
-          },
-          submit: {
-            type: 'plain_text',
-            text: 'Submit',
-          },
-          blocks: [
-            {
-              type: 'input',
-              block_id: 'form',
-              element: {
-                type: 'plain_text_input',
+          title: PlainText('SMS the Chorus'),
+          submit: PlainText('Submit'),
+          blocks: Blocks([
+            InputBlock(
+              'Message',
+              PlainTextInput('message', '', 'What do you want to send\n\n', {
                 multiline: true,
-                action_id: 'message',
-                placeholder: {
-                  type: 'plain_text',
-                  text: 'What do you want to send?\n\n',
-                },
-              },
-              label: {
-                type: 'plain_text',
-                text: 'Message',
-              },
-              hint: {
-                type: 'plain_text',
-                text: 'Your message will be prefixed with "HCA:" automatically',
-              },
-            },
-          ],
+              }),
+              'form',
+              PlainText(
+                'Your message will be prefixed with "HCA:" automatically',
+              ),
+            ),
+          ]),
           type: 'modal',
         },
-      });
-    } else {
-      await client.chat.postEphemeral({
-        channel: notificationChannel,
-        user: callingUser,
-        text: `You haven't been given access to text everyone. If you need access, ask in <#C01212HNHM2|board-discussions>.`,
+      }),
+      isUserAuthorized(client, callingUser),
+    ]);
+
+    const { view } = openResp;
+
+    if (!view) {
+      throw new Error('Could not get members or view');
+    }
+
+    // Check if user is a member of the channel to authorize the request
+    if (!userIsAuthorized) {
+      await client.views.update({
+        view_id: view.id,
+        view: {
+          type: 'modal',
+          title: PlainText('SMS the Chorus'),
+          blocks: Blocks([
+            MdSection(
+              `You haven't been given access to text everyone. If you need access, ask in <#C01212HNHM2|board-discussions>.`,
+            ),
+          ]),
+        },
       });
     }
   });
-
-  const harmonySiteNumbers = async (): Promise<string[]> => {
-    const hsUsername = process.env.HARMONYSITE_USERNAME || '';
-    const hsPassword = process.env.HARMONYSITE_PASSWORD || '';
-    await harmonySite.authorise(hsUsername, hsPassword);
-
-    const membersTable = await harmonySite.browse({
-      table: 'members',
-      n: 1000,
-    });
-
-    const membershipTable = await harmonySite.browse({
-      table: 'memberships',
-      n: 1000,
-    });
-
-    const memberMap = _.keyBy(
-      membersTable.records.member,
-      (member) => member.id,
-    );
-
-    const memberships = _.keyBy(
-      membershipTable.records.membership,
-      (member) => member.Member,
-    );
-
-    const members = membersTable.records.member
-      .filter(
-        (member: any) =>
-          memberships[member.id] &&
-          memberships[member.id].Type === 'Member' &&
-          memberships[member.id].Status === 'Active' &&
-          memberships[member.id].Level === 'Full',
-      )
-      .map((member: any) => {
-        return memberMap[member.id];
-      });
-
-    return members
-      .map(
-        (member: any) =>
-          member.MobilePhone || member.HomePhone || member.WorkPhone,
-      )
-      .filter(_.identity)
-      .filter((phone: any) => typeof phone === 'string')
-      .map((phone: string) => new PhoneNumber(phone, 'US'))
-      .filter((number: PhoneNumber) => number.isValid() && number.isPossible())
-      .map((number: PhoneNumber) => number.getNumber())
-      .filter(_.identity);
-  };
 
   app.view(messageViewId, async ({ ack, client, body, view }) => {
     await ack();
@@ -148,6 +156,16 @@ export const createApp = (receiver?: Receiver) => {
         channel: notificationChannel,
         user: user,
         text: `You didn't enter a message. Give it another go.`,
+      });
+      return;
+    }
+
+    const userIsAuthorized = await isUserAuthorized(client, user);
+
+    if (!userIsAuthorized) {
+      await client.chat.postMessage({
+        channel: notificationChannel,
+        text: `<@${user}> attempted to send a message to the chorus but isn't authorized.\n\n> ${message} `,
       });
       return;
     }
